6f903892239b6c98508815a3f0c87fbff19f0902
GHERGUT MADALIN <madalinghergut@gmail.com>
2023-08-13 14:06:55 +0300

    done with composer

---
+++ currency-exchange-service/src/main/java/main/repository/CurrencyExchangeRepository.java
@@ -0,0 +1,12 @@
+package main.repository;
+
+import main.bean.CurrencyExchange;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface CurrencyExchangeRepository extends JpaRepository<CurrencyExchange, Long> {
+
+    CurrencyExchange findByFromAndTo(String from, String to);
+
+}
